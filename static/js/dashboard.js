aab77aa6764d12074d524d0fe2009d17475fe494
Donald Huang <don.hcd@gmail.com>
2014-03-07 17:33:09 -0800

    add create license button

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,44 +1,61 @@
 var app = angular.module('myApp', ["trNgGrid"]);
 
 app.controller('LicenseViewerController', ['$scope', '$http', function($scope, $http) {
-	$http({
-		method:'GET',
-		url:'licenses'
-	}).success(function(data, status, headers, config) {
-		var licensesMap = {};
-		for (var i=0; i<data.length; i++) {
-			var license = data[i];
-			if (license.lastPing) {
-				license.lastPing *= 1000;
+	$scope.newlicensetype = 'enterprise';
+
+	function loadTheThings() {
+		$http({
+			method:'GET',
+			url:'licenses'
+		}).success(function(data, status, headers, config) {
+			var licensesMap = {};
+			for (var i=0; i<data.length; i++) {
+				var license = data[i];
+				if (license.lastPing) {
+					license.lastPing *= 1000;
+				}
+
+				license.isValid = !!license.isValid;
+
+				var storedLicense = licensesMap[license.id];
+				if (!storedLicense) {
+					licensesMap[license.id] = license;
+					storedLicense = license;
+				}
+				if (license.metadataName) {
+					storedLicense[license.metadataName] = parseInt(license.metadataValue, 10) || license.metadataValue;
+				}
+				if (license.permissionName) {
+					storedLicense[license.permissionName] = parseInt(license.permissionValue, 10) || license.permissionValue;
+				}
+
+				delete storedLicense.metadataName;
+				delete storedLicense.metadataValue;
+				delete storedLicense.permissionName;
+				delete storedLicense.permissionValue;
+				delete storedLicense.accountId;
+				delete storedLicense.stripeCustomerId;
+				delete storedLicense.unpaidExpiration;
 			}
 
-			license.isValid = !!license.isValid;
-
-			var storedLicense = licensesMap[license.id];
-			if (!storedLicense) {
-				licensesMap[license.id] = license;
-				storedLicense = license;
+			var licenses = [];
+			for (var id in licensesMap) {
+				licenses.push(licensesMap[id]);
 			}
-			if (license.metadataName) {
-				storedLicense[license.metadataName] = parseInt(license.metadataValue, 10) || license.metadataValue;
-			}
-			if (license.permissionName) {
-				storedLicense[license.permissionName] = parseInt(license.permissionValue, 10) || license.permissionValue;
-			}
+			$scope.licenses = licenses;
+		});
+	}
+	loadTheThings();
 
-			delete storedLicense.metadataName;
-			delete storedLicense.metadataValue;
-			delete storedLicense.permissionName;
-			delete storedLicense.permissionValue;
-			delete storedLicense.accountId;
-			delete storedLicense.stripeCustomerId;
-			delete storedLicense.unpaidExpiration;
-		}
-
-		var licenses = [];
-		for (var id in licensesMap) {
-			licenses.push(licensesMap[id]);
-		}
-		$scope.licenses = licenses;
-	});
+	$scope.makeLicense = function() {
+		$http({
+			method: 'POST',
+			url: 'license/generate',
+			data: {
+				type: $scope.newlicensetype
+			}
+		}).success(function(data, status, headers, config) {
+			loadTheThings();
+		});
+	}
 }]);
